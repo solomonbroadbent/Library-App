55dcf7a4a5e7e47eec821f7923fd1648ed5d5341
Solomon <solomonbroadbentnz@gmail.com>
2018-01-26 06:38:49 +1030

    Added progress from workshop 2

---
+++ app/controllers/index.js
@@ -0,0 +1,22 @@
+//import {computed, observer} from '@ember/object'
+import Controller from '@ember/controller';
+import { match, not } from '@ember/object/computed';
+
+export default Controller.extend({
+  isValid: match('emailAddress',  /^.+@.+\..+$/),
+  isDisabled: not('isValid'),
+  actions: {
+    saveInvitation() {
+      const verifiedEmailAddress = this.get('emailAddress');
+      this.set('responseMessage', `An invitation has been sent to: ${verifiedEmailAddress}`);
+      const invitation = this.store.createRecord('invitation', {
+        emailAddress: verifiedEmailAddress
+      });
+      invitation.save();
+    },
+    setEmailAddressToBlank() {
+      this.set('emailAddress', '');
+    }
+  }
+
+});
--- config/environment.js
+++ config/environment.js
@@ -6,6 +6,23 @@ module.exports = function(environment) {
     environment,
     rootURL: '/',
     locationType: 'auto',
+
+    firebase: {
+      apiKey: "AIzaSyB5DisWE4sRzl0Bg_737_t4OlePNVdSbt4",
+      authDomain: "library-app-61364.firebaseapp.com",
+      databaseURL: "https://library-app-61364.firebaseio.com",
+      projectId: "library-app-61364",
+      storageBucket: "library-app-61364.appspot.com",
+      messagingSenderId: "735501806734"
+    },
+
+    // if using ember-cli-content-security-policy
+    contentSecurityPolicy: {
+      'script-src': "'self' 'unsafe-eval' apis.google.com",
+      'frame-src': "'self' https://*.firebaseapp.com",
+      'connect-src': "'self' wss://*.firebaseio.com https://*.googleapis.com"
+    },
+
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
@@ -25,10 +42,10 @@ module.exports = function(environment) {
 
   if (environment === 'development') {
     // ENV.APP.LOG_RESOLVER = true;
-    // ENV.APP.LOG_ACTIVE_GENERATION = true;
-    // ENV.APP.LOG_TRANSITIONS = true;
-    // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
-    // ENV.APP.LOG_VIEW_LOOKUPS = true;
+    ENV.APP.LOG_ACTIVE_GENERATION = true;
+    ENV.APP.LOG_TRANSITIONS = true;
+    ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
+    ENV.APP.LOG_VIEW_LOOKUPS = true;
   }
 
   if (environment === 'test') {
